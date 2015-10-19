a6a1e084a729faee3aeaa203d8a4cf10f010f603
Frédéric Boulet <frederic.boulet@gmail.com>
2015-10-19 17:35:17 +0200

    limit to 10

--- migrate.js
+++ migrate.js
@@ -5,11 +5,13 @@ var util = require('util');
 var mongodb = require('mongodb');
 var uuid = require('node-uuid');
 var using = Promise.using;
+var request = request = Promise.promisify(require("request"));
 
 Promise.promisifyAll(fs);
 Promise.promisifyAll(mongodb);
 
 mongodb_uri  = process.env.MONGO_URI;
+ghost_url = process.env.GHOST_URL
 
 var idStart = 1000;
 
@@ -19,16 +21,27 @@ function getDatabase() {
   });
 }
 
+function getImport() {
+  // http://test.sizvideos.com/ghost/api/v0.1/authentication/token
+  var token =  process.env.AUTH_TOKEN;
+  return request({
+    url: ghost_url + '/ghost/api/v0.1/db/?access_token=' + token,
+    json: true
+    }).spread(function(response, body) {
+      return body;
+    });
+}
+
 function toGhostPost(from) {
   return { id: idStart++,
     uuid: uuid.v4(),
-    title: "zzz - " + from.title,
-    slug: "yyy - " + from.slug,
+    title: 'import-' + from.title,
+    slug: 'import-' + from.slug,
     markdown: from.title,
     html: '<p>'+ from.title + '</p>' +
           '<p>dont la source est ' + from.source.id + '</p>' +
           '<p>et les video sont là: ' + from.boxes.map(function(box){return 'http:' + box.formats[0].href}).join() + '</p>',
-    image: from.picture,
+    image: 'http://' + from.picture.href,
     featured: 0,
     page: 0,
     status: 'published',
@@ -47,15 +60,15 @@ function toGhostPost(from) {
 
 using(getDatabase(), function(db) {
   return Promise.all([
-    fs.readFileAsync('source.json', 'utf-8').then(JSON.parse),
-    db.collection("stories").find({}).toArrayAsync()
+    getImport(),
+    db.collection("stories").find({}, {limit: 10}).toArrayAsync()
   ])
 }).then(function(results) {
     console.log(results[0].db[0].data.posts);
     results[0].db[0].data.posts = results[0].db[0].data.posts.concat(results[1].map(toGhostPost));
     return results[0];
 }).then(function(writeMe){
-    return fs.writeFileAsync('output.json', JSON.stringify(writeMe), {} );
+    return fs.writeFileAsync('output.json', JSON.stringify(writeMe, null, 2), {} );
    })
   .then(function() {
     console.log('fini');
